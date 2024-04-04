0f636dcdad35c8e60ecf122f127b939b1f1a2e6b
Sarth Patel <sarthbpatel@gmail.com>
2024-04-03 23:33:59 -0400

    Updated RoadMap Styling

--- app/client-components/Roadmap.tsx
+++ app/client-components/Roadmap.tsx
@@ -1,20 +1,23 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Capability, TaskOrder } from "./data-manipulation";
-import { Tooltip } from 'react-tooltip';
+import { Tooltip } from "react-tooltip";
 import html2canvas from "html2canvas";
-import 'react-tooltip/dist/react-tooltip.css'
+import "react-tooltip/dist/react-tooltip.css";
 
-export default function Roadmap( {taskOrders} : { taskOrders : TaskOrder[]}) {
-  const nextEightQuarters = getNextEightQuarters()
+export default function Roadmap({ taskOrders }: { taskOrders: TaskOrder[] }) {
+  const nextEightQuarters = getNextEightQuarters();
+  const componentRef = useRef<HTMLDivElement>(null);
+  const [isFullScreen, setIsFullScreen] = useState(true);
+
   function getNextEightQuarters() {
     //first get the date
-    const today = new Date()
+    const today = new Date();
     //then get the current year
-    const currentYear = today.getFullYear()
+    const currentYear = today.getFullYear();
     //now get the last 2 digits of the year sliced from the string
-    const lastTwoDigits = currentYear.toString().slice(2)
-    const lastTwoDigitsAsNumber = parseInt(lastTwoDigits)
-  
+    const lastTwoDigits = currentYear.toString().slice(2);
+    const lastTwoDigitsAsNumber = parseInt(lastTwoDigits);
+
     return [
       `FY${lastTwoDigits}Q1`,
       `FY${lastTwoDigits}Q2`,
@@ -23,68 +26,90 @@ export default function Roadmap( {taskOrders} : { taskOrders : TaskOrder[]}) {
       `FY${lastTwoDigitsAsNumber + 1}Q1`,
       `FY${lastTwoDigitsAsNumber + 1}Q2`,
       `FY${lastTwoDigitsAsNumber + 1}Q3`,
-      `FY${lastTwoDigitsAsNumber + 1}Q4`
-    ]
+      `FY${lastTwoDigitsAsNumber + 1}Q4`,
+    ];
   }
 
-  const componentRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsFullScreen(window.innerHeight === screen.height);
+    };
 
-  async function handleScreenshot(e : any) {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  async function handleScreenshot(e: any) {
     e.preventDefault();
-    console.log('Taking screenshot')
+    console.log("Taking screenshot");
     if (componentRef.current) {
       try {
-        console.log('Taking screenshot')
+        console.log("Taking screenshot");
         const canvas = await html2canvas(componentRef.current);
         // Create a link to download the canvas image
-        const link = document.createElement('a');
-        link.download = 'screenshot.png';
-        link.href = canvas.toDataURL('image/png');
+        const link = document.createElement("a");
+        link.download = "screenshot.png";
+        link.href = canvas.toDataURL("image/png");
         link.click();
       } catch (error) {
-        console.error('Error taking screenshot:', error);
+        console.error("Error taking screenshot:", error);
       }
     }
   }
 
   return (
-    <div ref={componentRef} className='w-full h-full flex flex-col bg-slate-900'>
+    <div
+      ref={componentRef}
+      className="w-full h-screen flex flex-col bg-slate-900"
+    >
       <Title screenshotFunction={handleScreenshot} />
       <HeaderRow nextEightQuarters={nextEightQuarters} />
-      {taskOrders ? taskOrders.map((to, index) => <TaskOrderDisplay key={index} taskOrder={to} index={index} nextEightQuarters={nextEightQuarters} />) : null}
+      <div className="flex-1 overflow-auto">
+        {taskOrders
+          ? taskOrders.map((to, index) => (
+              <TaskOrderDisplay
+                key={index}
+                taskOrder={to}
+                index={index}
+                nextEightQuarters={nextEightQuarters}
+                isFullScreen={isFullScreen}
+              />
+            ))
+          : null}
+      </div>
     </div>
-  )
+  );
 }
 
-function Title({screenshotFunction}: {screenshotFunction: (e : any) => void}) {
-  const currentYear = new Date().getFullYear()
-  const previousYear = currentYear - 1
-  const nextYear = currentYear + 1
+function Title({
+  screenshotFunction,
+}: {
+  screenshotFunction: (e: any) => void;
+}) {
+  const currentYear = new Date().getFullYear();
+  const previousYear = currentYear - 1;
+  const nextYear = currentYear + 1;
 
   return (
-    <form
-      className="text-4xl w-full px-5"
-    >
-      <div
-        className="flex flex-row justify-between w-full"
-      >
+    <form className="text-4xl w-full px-5">
+      <div className="flex flex-row justify-between w-full">
         <div className="flex flex-row">
-          <div className="font-bold m-2">
-            Roadmap:
-          </div>
-          <div
-            className="m-2"
-          >
+          <div className="font-bold m-2">Roadmap:</div>
+          <div className="m-2">
             October {previousYear} - October {nextYear}
           </div>
         </div>
-        <button 
+        <button
           className="text-sm bg-blue-500 rounded-md p-1 m-2 hover:bg-blue-700 hover:text-white"
           onClick={screenshotFunction}
         >
           Save as Image
         </button>
-        {/* <button 
+        {/* <button
           className="text-xs bg-blue-500 rounded-md p-1 m-2 hover:bg-blue-700 hover:text-white"
           onClick={toggleExpanded}
         >
@@ -92,29 +117,56 @@ function Title({screenshotFunction}: {screenshotFunction: (e : any) => void}) {
         </button> */}
       </div>
     </form>
-  )
+  );
 }
 
 function HeaderRow({ nextEightQuarters }: { nextEightQuarters: string[] }) {
   return (
-    <div
-      className="h-8 flex flex-row"
-    >
+    <div className="h-8 flex flex-row">
       <div className="flex-1"></div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-900">{nextEightQuarters[0]}</div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-700">{nextEightQuarters[1]}</div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-500">{nextEightQuarters[2]}</div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-300">{nextEightQuarters[3]}</div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-900">{nextEightQuarters[4]}</div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-700">{nextEightQuarters[5]}</div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-500">{nextEightQuarters[6]}</div>
-      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-300">{nextEightQuarters[7]}</div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-900">
+        {nextEightQuarters[0]}
+      </div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-700">
+        {nextEightQuarters[1]}
+      </div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-500">
+        {nextEightQuarters[2]}
+      </div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-300">
+        {nextEightQuarters[3]}
+      </div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-900">
+        {nextEightQuarters[4]}
+      </div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-700">
+        {nextEightQuarters[5]}
+      </div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-500">
+        {nextEightQuarters[6]}
+      </div>
+      <div className="flex-1 flex flex-col text-center rounded-md bg-purple-300">
+        {nextEightQuarters[7]}
+      </div>
     </div>
-  )
+  );
 }
 
-function TaskOrderDisplay({ taskOrder, index, nextEightQuarters }: { taskOrder: TaskOrder, index: number, nextEightQuarters: string[]}) {
-  const capabilities = taskOrder.portfolioEpics.map(pe => pe.capabilities).flat()
+function TaskOrderDisplay({
+  taskOrder,
+  index,
+  nextEightQuarters,
+  isFullScreen,
+}: {
+  taskOrder: TaskOrder;
+  index: number;
+  nextEightQuarters: string[];
+  isFullScreen: boolean;
+}) {
+  const capabilities = taskOrder.portfolioEpics
+    .map((pe) => pe.capabilities)
+    .flat();
+
   function getShadeOfPurple(index: number) {
     const shadesOfPurple = [
       "#4B0082", // Indigo
@@ -126,34 +178,75 @@ function TaskOrderDisplay({ taskOrder, index, nextEightQuarters }: { taskOrder: 
     ];
     return shadesOfPurple[index];
   }
-  const color = getShadeOfPurple(index)
-  const firstColumn = '1'
-  const numberOfRows = capabilities.length
-  const gridRowForTaskOrder = `1 / ${2 + numberOfRows}`
+  const color = getShadeOfPurple(index);
+  const firstColumn = "1";
+  const numberOfRows = capabilities.length;
+  const gridRowForTaskOrder = `1 / ${2 + numberOfRows}`;
 
   return (
-    <div className='flex-1 flex flex-row w-full'
-      key={index}
-    >
-      <div className='h-full w-full grid grid-cols-9 bg-gray-800'>
-        <div style={{ gridColumn: firstColumn, gridRow: gridRowForTaskOrder, backgroundColor: color }}>{taskOrder.name}</div>
-        {capabilities.map((c, i) => <CapabilityDisplay key={i} capability={c} index={i + 1} color={color} nextEightQuarters={nextEightQuarters} />)}
+    <div className="flex-1 flex flex-row w-full" key={index}>
+      <div className="h-full w-full grid grid-cols-9 bg-gray-800">
+        <div
+          style={{
+            gridColumn: firstColumn,
+            gridRow: gridRowForTaskOrder,
+            backgroundColor: color,
+          }}
+        >
+          {taskOrder.name}
+        </div>
+        {capabilities.map((c, i) => (
+          <CapabilityDisplay
+            key={i}
+            capability={c}
+            index={i + 1}
+            color={color}
+            nextEightQuarters={nextEightQuarters}
+            isFullScreen={isFullScreen}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-function CapabilityDisplay({ capability, index, color, nextEightQuarters }: { capability: Capability, index: number, color: string, nextEightQuarters: string[]}) {
-  const [expanded, setExpanded] = useState(false)
-  function toggleExpanded() { setExpanded(!expanded) }
+function CapabilityDisplay({
+  capability,
+  index,
+  color,
+  nextEightQuarters,
+  isFullScreen,
+}: {
+  capability: Capability;
+  index: number;
+  color: string;
+  nextEightQuarters: string[];
+  isFullScreen: boolean;
+}) {
+  const [expanded, setExpanded] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  const gridRowIndex = index + 1
-  let gridRow = `${gridRowIndex} / ${gridRowIndex + 1}`
-  let gridColumn = getGridColumns(nextEightQuarters, capability.labels)
-  function getGridColumns(
-    getNextEightQuarters: string[],
-    labels: string[]
-  ) {
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      setExpanded(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  const gridRowIndex = index + 1;
+  let gridRow = `${gridRowIndex} / ${gridRowIndex + 1}`;
+  let gridColumn = getGridColumns(nextEightQuarters, capability.labels);
+
+  function getGridColumns(getNextEightQuarters: string[], labels: string[]) {
     const beginningQuarter = labels[0];
     const endQuarter = labels[labels.length - 1];
     const beginningIndex = getNextEightQuarters.indexOf(beginningQuarter);
@@ -161,23 +254,36 @@ function CapabilityDisplay({ capability, index, color, nextEightQuarters }: { ca
     const gridColumn = `${beginningIndex + 2} / ${endIndex + 3}`;
     return gridColumn;
   }
-  if (gridColumn === '1 / 2') {
-    return <></>
+
+  if (gridColumn === "1 / 2") {
+    return <></>;
   }
+
+  const maxWidth = window.screen.width;
+  const textLength = capability.name.length;
+  const visibleLength = Math.floor((windowWidth / maxWidth) * textLength);
+  const truncatedText = capability.name.slice(0, visibleLength);
+
+  const isTextTruncated = visibleLength < textLength;
+
   return (
     <div
-      className={`rounded-md ${ expanded ? '' : 'h-4 overflow-ellipsis'} m-0 text-xs border px-1 overflow-hidden cursor-s-resize`}
-      onClick={toggleExpanded}
+      className={`rounded-md h-auto py-auto m-0 text-xs border px-1 ${
+        isTextTruncated && !isFullScreen ? "cursor-pointer" : ""
+      }`}
+      onClick={isTextTruncated && !isFullScreen ? toggleExpanded : undefined}
       style={{
         gridColumn: gridColumn,
         gridRow: gridRow,
-        backgroundColor: color
-      }}>
-      {capability.name}
-      <div className="absolute text-xs rounded bg-black text-white p-1 bottom-full mb-2 group-hover:visible">
-        text
+        backgroundColor: color,
+        width: "100%",
+        height: "100%",
+        margin: "0",
+      }}
+    >
+      <div className="whitespace-normal break-words">
+        {isFullScreen || expanded ? capability.name : truncatedText}
       </div>
     </div>
-  )
-
+  );
 }
