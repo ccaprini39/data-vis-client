15ff46c7c52617c56cb0d4fa827ed4a07b4eceb2
Clouis <ccaprini39@gmail.com>
2024-05-08 06:17:10 -0400

    added in tabs, moved things around a bit

--- app/client-components/MainApplicationPage.tsx
+++ app/client-components/MainApplicationPage.tsx
@@ -3,6 +3,9 @@ import { parseXLSFile, TaskOrder } from "./data-manipulation";
 import Roadmap from "./Roadmap";
 import { ModeToggle } from "@/components/ModeToggleButton";
 
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+
+
 export default function MainApplicationPage() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [tableData, setTableData] = useState<TaskOrder[]>([]);
@@ -27,6 +30,49 @@ export default function MainApplicationPage() {
     const file = event.target.files ? event.target.files[0] : null;
     setSelectedFile(file);
   }
+
+  return (
+    <div className="w-full h-full m-5">
+      <Tabs defaultValue="file" className="w-full">
+        <TabsList className="flex flex-row justify-between">
+          <div className="flex gap-1">
+            <TabsTrigger value="file">File</TabsTrigger>
+            <TabsTrigger disabled={tableData.length === 0} value="roadmap">Roadmap</TabsTrigger>
+            <TabsTrigger disabled={tableData.length === 0} value="epic">Epics View</TabsTrigger>
+          </div>
+          <ModeToggle />
+        </TabsList>
+        <TabsContent value="file" className="p-4">
+          <label htmlFor="fileInput" className="block">
+            <span className="sr-only">Choose file</span>
+            <input
+              type="file"
+              id="fileInput"
+              onChange={handleChange}
+              accept=".xls,.xlsx"
+              className="block w-full mb-4 mx-2 text-sm text-gray-400 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-purple-50 file:text-purple-700 hover:file:bg-gray-300"
+            />
+          </label>
+          <button
+            className="px-4 py-2 mb-4 text-sm font-medium text-white bg-purple-500 rounded-md hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
+            onClick={handleResetFileSelection}
+          >
+            Reset
+          </button>
+        </TabsContent>
+        <TabsContent value="roadmap" className="p-4">
+          {tableData.length === 0 ? (
+            <div className="mx-4">No data</div>
+          ) : (
+            <Roadmap taskOrders={tableData} />
+          )}
+        </TabsContent>
+        <TabsContent value="epic" className="p-4">
+          <div>Epics View</div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
 
   return (
     <div className="w-full h-full m-5">
